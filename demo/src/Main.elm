c87da16db7d2c966595534df6e9548b09ebb5556
Anton Ovechkin <owanturist@gmail.com>
2020-01-15 22:54:18 +0700

    Add demo for checking elm debugger compatibility

---
+++ demo/src/Main.elm
@@ -0,0 +1,65 @@
+module Main exposing (main)
+
+import AVL.Dict as Dict exposing (Dict)
+import Browser
+import Html exposing (Html, text)
+
+
+
+-- ID
+
+
+type ID
+    = ID Int
+
+
+compareID : ID -> ID -> Order
+compareID (ID left) (ID right) =
+    compare left right
+
+
+
+-- U S E R
+
+
+type alias User =
+    { id : ID
+    , name : String
+    , age : Int
+    , height : Float
+    }
+
+
+
+-- M O D E L
+
+
+type alias Model =
+    Dict ID User
+
+
+init : Model
+init =
+    Dict.fromListWith compareID
+        [ ( ID 0, User (ID 0) "Alice" 28 1.65 )
+        , ( ID 1, User (ID 1) "Bob" 19 1.82 )
+        , ( ID 2, User (ID 2) "Chuck" 33 1.75 )
+        ]
+
+
+
+-- V I E W
+
+
+{-| It helps to check that Elm Debugger shows Dict correctly.
+
+    elm make --debug src/Main.elm
+
+-}
+main : Program () Model msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = \_ model -> model
+        , view = always (text "")
+        }
